42e7fc974d886ae9b07a2068632ea9350b544b7b
STH-Dev <patrick@servethehome.com>
2014-05-27 09:08:05 -0700

    Added header block comments

--- STHbench-Dev012.11.sh
+++ STHbench-Dev012.11.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 #
-#		(C) 2013-2014 ServeTheHome.com and ServeThe.biz
+#	(C) 2013-2014 ServeTheHome.com and ServeThe.biz
 #		
 #
-# 	STHbench - A System Benchmark and comparison tool created by the STH community.
+# 	STHbench2- A System Benchmark and comparison tool created by the STH community.
 #
-#	Should include a description of what the script does right here... 
-#	All the benchmarks it includes... packages it installs... actions during runtime etc.
+#	STHbench2 is a sscript that runs hardinfo, Unixbench 5.1.3, c-ray 1.1, STREAM, OpenSSL, sysbench (CPU),
+#	crafty, redis, NPB, NAMD, and 7-zip benchmarks
+#	
+#	STHbench2 must be run as root or using a su prompt to automate download and installation of benchmarks
 #
 #	For more information go:
 #	http://forums.servethehome.com/processors-motherboards/2519-introducing-sthbench-sh-benchmark-script.html
 #
-# 	Authors: Patrick Kennedy, nitrobass24, mir, Chuckleb, Patriot 
+# 	Authors: Patrick Kennedy, Chuckleb, Patriot, nitrobass24, mir  
 #
 #
 #	Latest development versions are available on the GitHub site:  https://github.com/STH-Dev/STHbench.sh
 #
-#   If you find bugs, verify you are on the latest version and then post in:
+#   	If you find bugs, verify you are on the latest version and then post in:
 #	http://forums.servethehome.com/index.php?threads/introducing-the-sthbench-sh-benchmark-script.2519/
 #
 ################################################################################################################################
