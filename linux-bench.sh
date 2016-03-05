b1bf173ea3f6021ba050deaf582f6386a55ff2da
STH-Dev <patrick@servethehome.com>
2016-03-05 14:24:32 -0800

    Update linux-bench.sh
    Updated with new file server

--- linux-bench.sh
+++ linux-bench.sh
@@ -25,7 +25,7 @@
 ################################################################################################################################
 
 #Current Version
-rev='12.17'
+rev='12.18'
 
 version()
 {
@@ -299,7 +299,7 @@ ubench()
 	cd $benchdir
 	echo "Building UnixBench"
 	wget -N https://byte-unixbench.googlecode.com/files/UnixBench5.1.3.tgz 
-	wget -N http://linuxbench.servethehome.com/benchfiles/fix-limitation.patch 
+	wget -N http://files.linux-bench.com/lb/fix-limitation.patch 
 	tar -zxf UnixBench5.1.3.tgz
 	
 	cd UnixBench 
@@ -430,7 +430,7 @@ red()
 	tar xzf redis-stable.tar.gz && cd redis-stable && make install
 	cp utils/redis_init_script /etc/init.d/redis_6379
 	mkdir -p /var/redis/6379
-	wget http://linuxbench.servethehome.com/benchfiles/6379.conf
+	wget http://files.linux-bench.com/lb/6379.conf
 	mkdir -p /etc/redis
 	cp ./6379.conf /etc/redis
 
@@ -477,7 +477,7 @@ NPB()
 
 	apptgz=NPB3.3.1.tar.gz
 	appbin=NPB3.3.1/NPB3.3-OMP
-	appdlpath=http://linuxbench.servethehome.com/benchfiles/$apptgz
+	appdlpath=http://files.linux-bench.com/lb/$apptgz
 	tgzstring=xfz
 	extract
 	
@@ -522,14 +522,14 @@ NAMD()
 	apptgz=NAMD_2.9_Linux-x86_64-multicore.tar.gz
 	tgzstring=xfz
 	appbin=$appbase/namd2
-	appdlpath=http://linuxbench.servethehome.com/benchfiles/$apptgz
+	appdlpath=http://files.linux-bench.com/lb/$apptgz
 	extract
 	
 	appbase=apoa1
 	apptgz=apoa1.tar.gz
 	tgzstring=xfz
 	appbin=$appbase/apoa1.pdb
-	appdlpath=http://linuxbench.servethehome.com/benchfiles/$apptgz
+	appdlpath=http://files.linux-bench.com/lb/$apptgz
 	extract
 
 	echo "Using" $threads "threads"
@@ -554,7 +554,7 @@ p7zip()
 	apptgz=p7zip_9.20.1_src_all.tar.bz2
 	tgzstring=xfj
 	appbin=p7zip_9.20.1/bin/7za
-	appdlpath=http://linuxbench.servethehome.com/benchfiles/$apptgz
+	appdlpath=http://files.linux-bench.com/lb/$apptgz
 	extract
 
 	echo "Building p7zip"
